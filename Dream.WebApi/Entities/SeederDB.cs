51745afda0137f4ae3534bf9e710d066b285fa32
Andriy23780 <Andriy23780@gmail.com>
2020-09-14 19:13:42 +0300

    Add db

---
+++ Dream.WebApi/Entities/SeederDB.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dream.WebApi.Entities
+{
+    public class SeederDB
+    {
+        public static void SeedData(UserManager<DbUser> userManager,
+                   RoleManager<DbRole> roleManager)
+        {
+            var email = "admin@gmail.com";
+            var roleName = "Admin";
+            var findUser = userManager.FindByEmailAsync(email).Result;
+            if (findUser == null)
+            {
+                var user = new DbUser
+                {
+                    Email = email,
+                    UserName = email,
+                    Image = "https://cdn.pixabay.com/photo/2017/07/28/23/34/fantasy-picture-2550222_960_720.jpg",
+                    Age = 30,
+                    Phone = "+380957476156",
+                    Description = "PHP programmer"
+                };
+                var result = userManager.CreateAsync(user, "Qwerty1-").Result;
+
+                var roleresult = roleManager.CreateAsync(new DbRole
+                {
+                    Name = roleName
+
+                }).Result;
+
+                result = userManager.AddToRoleAsync(user, roleName).Result;
+            }
+        }
+
+        public static void SeedDataByAS(IServiceProvider services)
+        {
+            using (var scope = services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var manager = scope.ServiceProvider.GetRequiredService<UserManager<DbUser>>();
+                var managerRole = scope.ServiceProvider.GetRequiredService<RoleManager<DbRole>>();
+                SeederDB.SeedData(manager, managerRole);
+            }
+        }
+    }
+
+}
