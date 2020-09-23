da2dddfcb2c2ad8eee261505a66c9a8ce1f40266
Andriy23780 <Andriy23780@gmail.com>
2020-09-23 20:23:32 +0300

    add retrofit get data

--- Deam.Android/app/src/main/java/com/example/dream/MainActivity.java
+++ Deam.Android/app/src/main/java/com/example/dream/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.dream;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.TextViewCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.dream.network.NetworkService;
+import com.example.dream.network.Post;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textVtestGet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +28,41 @@ public class MainActivity extends AppCompatActivity {
         //Log.e("Semen","----Hello Peter-----");
         //Log.d("slava", "------dispaly----");
         setContentView(R.layout.activity_main);
+        textVtestGet = findViewById(R.id.textVtestGet);
     }
 
-    public void btnClickPlus(View v)
-    {
+    public void btnClickPlus(View v) {
         Toast toast = Toast.makeText(getApplicationContext(),
                 "Нажатий плюс", Toast.LENGTH_LONG);
         toast.show();
+    }
+
+    public void btnClickcalculator(View v) {
+        Intent intent = new Intent(this, CalculatorActivity.class);
+        startActivity(intent);
+    }
+
+    public void TestGet(View v) {
+        NetworkService.getInstance()
+                .getJSONApi()
+                .getPostWithID(1)
+                .enqueue(new Callback<Post>() {
+                    @Override
+                    public void onResponse(@NonNull Call<Post> call, @NonNull Response<Post> response) {
+                        Post post = response.body();
+
+                        textVtestGet.append(post.getId() + "\n");
+                        textVtestGet.append(post.getUserId() + "\n");
+                        textVtestGet.append(post.getTitle() + "\n");
+                        textVtestGet.append(post.getBody() + "\n");
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<Post> call, @NonNull Throwable t) {
+
+                        textVtestGet.append("Error occurred while getting request!");
+                        t.printStackTrace();
+                    }
+                });
     }
 }
