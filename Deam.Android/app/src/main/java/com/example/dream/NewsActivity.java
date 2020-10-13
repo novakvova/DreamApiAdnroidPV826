4d6ccaeabb8d18f3b53ac59c993ed06a3a72ad8a
Volodymyr Novak <novakvova@gmail.com>
2020-10-13 18:29:37 +0300

    Recucler view

--- Deam.Android/app/src/main/java/com/example/dream/NewsActivity.java
+++ Deam.Android/app/src/main/java/com/example/dream/NewsActivity.java
@@ -30,13 +30,19 @@ public class NewsActivity extends AppCompatActivity {
 
     private void setRecyclerView() {
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 2,
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1,
                 GridLayoutManager.VERTICAL, false));
         list = new ArrayList<>();
         NewsModel model = new NewsModel("https://www.growthengineering.co.uk/wp-content/uploads/2014/07/To-Do-List.png", "Hello");
         list.add(model);
+        NewsModel model1 = new NewsModel("https://i.guim.co.uk/img/media/20098ae982d6b3ba4d70ede3ef9b8f79ab1205ce/0_0_969_581/master/969.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=a368f449b1cc1f37412c07a1bd901fb5", "Навала короновіруса");
+        list.add(model1);
         adapter = new NewsCardRecyclerViewAdapter(list);
 
         recyclerView.setAdapter(adapter);
+
+        int largePadding = 16;
+        int smallPadding = 4;
+        recyclerView.addItemDecoration(new NewsGridItemDecoration(largePadding, smallPadding));
     }
 }
