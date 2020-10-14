7789076f895d3c22a70cd03cbf95905a60130b9c
Andriy23780 <Andriy23780@gmail.com>
2020-10-14 20:26:33 +0300

    gut list univ begin 2

--- Deam.Android/app/src/main/java/com/example/dream/network/University.java
+++ Deam.Android/app/src/main/java/com/example/dream/network/University.java
@@ -1,4 +1,51 @@
 package com.example.dream.network;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class University {
+    @SerializedName("id")
+    @Expose
+    private int id;
+    @SerializedName("name")
+    @Expose
+    private int name;
+    @SerializedName("image")
+    @Expose
+    private int image;
+    @SerializedName("cityId")
+    @Expose
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
 }
